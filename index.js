258129dcba3d4d5463c828ec8f85fb5cc7d8defa
Gustavo <gumrqss@gmail.com>
2022-08-28 18:53:11 -0300

    Get tweets finalizado

--- index.js
+++ index.js
@@ -27,15 +27,41 @@ server.post("/tweets", (req, res)=> {
     const user = req.body.username;
 console.log(user, "USER DO TWEETS")
     const isUser = users.find((value) => value.username === user);
-    if(!isUser || !tweet.tweet){
-        return res.status(400).send('Unprocessable')
+    if(!isUser) {
+        return res.status(404).send('User not found')
+    }
+    if(!tweet.tweet){
+        return res.status(422).send("Tweet cannot be empty")
+
     }
     usersTweets.push(tweet);
     return res.status(201).send('OK');
 });
 
 
+server.get("/tweets", (req,res)=> {
+    const tweets =  usersTweets.map((usersTweet) => {
+        let avatar;
 
+        users.forEach((user)=> {
+            if (user.username === usersTweet.username){
+                avatar = user.avatar;
+            }
+        });
+
+        return {
+            username: usersTweet.username,
+            avatar: avatar,
+            tweet: usersTweet.tweet
+        }
+    });
+
+    const lastTweets= tweets.slice(tweets.length-10)
+    const invertedLastTweets = lastTweets.reverse()
+
+
+    res.status(200).send(invertedLastTweets);
+});
 
 
 
